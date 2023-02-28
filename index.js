df512b3c29a26423e52884b15ca59f350920b908
KrYounger <35814586+xt0fer@users.noreply.github.com>
2023-02-28 20:37:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} change
+ * @return {number}
+ */
+var coinChange = function(change) {
+    let quarter = 25;
+    let dime = 10;
+    let nickel = 5;
+    let penny = 1;
+    let result = [0,0,0,0];
+
+    // your code here.
+
+    return result;
+};
+
+
+module.exports = {
+  coinChange
+}
